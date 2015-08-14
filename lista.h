1ccdb52796401c767af5798bd2ec36451df7d3cd
lezs <les.mg92@gmail.com>
2015-08-13 22:24:27 -0600

    j

--- lista.h
+++ lista.h
@@ -1,5 +1,119 @@
 #ifndef LISTA
 #define LISTA
 
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct nodo{
+    nodo* siguiente;
+    nodo* anterior;
+    int dato;
+
+    nodo(int dato)
+    {
+        this->dato = dato;
+        siguiente = anterior = NULL;
+    }
+};
+
+
+struct lista{
+    nodo* cabeza;
+    nodo* cola;
+
+    lista(){
+        cabeza = cola = NULL;
+            }
+
+
+    bool vacia(){
+        if (cabeza==NULL){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    int Get(){
+        if(!vacia()){
+            int val = cabeza->dato;
+            nodo *bor = cabeza;
+                    if (cabeza == cola)
+                    {
+                        cabeza = NULL;
+                        cola = NULL;
+                    }
+                    else
+                    {
+                        cabeza = cabeza->siguiente;
+                    }
+                    delete bor;
+                    return val;
+                }
+                else
+                    return -1;
+        }
+
+
+
+
+    void Insertar(nodo* nod){
+            if(vacia()){
+                cabeza = cola = nod;
+            }  else {
+                if(nod->dato < cabeza->dato){
+                    InsertarFrente(nod);
+                }else if(nod->dato > cola->dato){
+                    InsertarAtras(nod);
+                }else{
+                    InsertarEnmedio(nod);
+                }
+            }
+    }
+
+     void InsertarFrente(nodo* nod){
+        cabeza->anterior = nod;
+        nod->siguiente = cabeza;
+        cabeza = cabeza->anterior;
+    }
+
+    void InsertarAtras(nodo* nod){
+        cola->siguiente = nod;
+        nod->anterior = cola;
+        cola = cola->siguiente;
+    }
+
+    void InsertarEnmedio(nodo* nod){
+        nodo* temp1;
+        nodo* temp2;
+        temp1 = cabeza;
+
+        while(temp1->dato < nod->dato){
+            temp1 = temp1->siguiente;
+           }
+
+        temp2 = temp1->anterior;
+        temp2->siguiente = nod;
+        temp1->anterior = nod;
+        nod->anterior = temp2;
+        nod->siguiente = temp1;
+    }
+
+    void Recorrer(){
+        nodo* temp;
+
+        temp = cabeza;
+
+        while(temp){
+            cout << "el valor es: " << temp->dato << endl;
+            temp = temp->siguiente;
+        }
+    }
+
+};
+
+
 #endif // LISTA
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <QCoreApplication>
+#include "lista.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+
+    lista* lis = new lista();
+
+    lis->Insertar(new nodo(2));
+    lis->Insertar(new nodo(9));
+    lis->Insertar(new nodo(3));
+
+    lis->Recorrer();
+
+    cout << lis->Get() << endl;
+
+    lis->Recorrer();
+
+    cout << lis->Get() << endl;
+
+    lis->Recorrer();
+
 
     return a.exec();
 }
